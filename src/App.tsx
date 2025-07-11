38aa8ce4213aa8134d98b867b52d3456449c084f
prave01 <praveenthanikachalam408@gmail.com>
2025-07-11 16:37:00 +0530

    Configure Theme 😉

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 import Background from "./Components/Background/Background";
@@ -6,27 +6,33 @@ import Morning from "./Components/Template/Morning";
 import Night from "./Components/Template/Night";
 
 function App() {
-  const [globalTheme, setTheme] = useState<"morning" | "night">("night");
+  const [globalTheme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
 
   const handleClick = () => {
-    setTheme(globalTheme == "night" ? "morning" : "night");
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
   };
 
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", globalTheme);
+    localStorage.setItem("theme", globalTheme);
+  }, [globalTheme]);
+
   return (
-    <div className="">
+    <div className={globalTheme}>
       <div className="absolute w-full h-full">
         <Background key={globalTheme} theme={globalTheme} />
         <div
           onClick={handleClick}
           className="pl-4 pt-14 w-fit relative cursor-pointer"
         >
-          {globalTheme == "morning" ? <Morning /> : <Night />}
+          {globalTheme === "light" ? <Morning /> : <Night />}
         </div>
       </div>
+
       <div className="w-full h-full flex items-center justify-center">
-        <h1 className="font-medium text-white/40 text-2xl">
-          This is the Title
-        </h1>
+        <h1 className="font-medium text-primary text-2xl">This is the Title</h1>
       </div>
     </div>
   );
--- src/Components/Background/Background.tsx
+++ src/Components/Background/Background.tsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 
 const Background = ({ theme }: { theme: string }) => {
   const [overlaybg, setOverlaybg] = useState<string>(
-    theme == "morning" ? "bg-[#010d08]" : "bg-[#f5e6c4]",
+    theme == "light" ? "bg-[#010d08]" : "bg-[#f5e6c4]",
   );
 
   console.log(overlaybg);
 
   const onComplete = () => {
     console.log(overlaybg);
-    theme == "morning"
+    theme == "light"
       ? setOverlaybg("bg-[#f5e6c4]")
       : setOverlaybg("bg-[#010d08]");
   };
@@ -22,14 +22,14 @@ const Background = ({ theme }: { theme: string }) => {
       <motion.div
         className="w-screen h-screen absolute flex items-start justify-start -z-10"
         style={{
-          background: `radial-gradient(circle at left 0%,${theme == "night" ? "#010d08" : "#f5e6c4"} 10%, transparent 1%)`,
+          background: `radial-gradient(circle at left 0%,${theme == "dark" ? "#010d08" : "#f5e6c4"} 10%, transparent 1%)`,
           backgroundRepeat: "no-repeat",
           backgroundSize: "10% 10%",
         }}
         key={theme}
         animate={{
           backgroundSize: ["10% 10%", "1000% 1000%"],
-          transition: { duration: 1},
+          transition: { duration: 1 },
         }}
         onAnimationComplete={onComplete}
       ></motion.div>
