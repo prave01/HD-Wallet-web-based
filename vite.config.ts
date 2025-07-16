8404245877e06b4a21cc78172283337828c4535b
prave01 <praveenthanikachalam408@gmail.com>
2025-07-16 19:57:47 +0530

    Config Changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,22 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import serverActions from "vite-plugin-server-actions";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    serverActions({
+      include: path.resolve(__dirname, "./src/Actions/**"),
+    }),
+    nodePolyfills(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
+
