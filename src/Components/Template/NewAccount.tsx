c609e313e52f5457594ece0d2f2655ee054551cd
prave01 <praveenthanikachalam408@gmail.com>
2025-07-18 14:04:17 +0530

    Update: NewAccount

--- src/Components/Template/NewAccount.tsx
+++ src/Components/Template/NewAccount.tsx
@@ -5,43 +5,37 @@ import { useState } from "react";
 import ComponentMap from "../Molecules/ComponentMap";
 
 const NewAccount = () => {
-	const [currentComp, setCurrentComp] = useState<{ name: string; idx: number }>(
-		{
-			name: "GenerateMnemonics",
-			idx: 0,
-		},
-	);
+  const [currentComp, setCurrentComp] = useState<number>(0);
 
-	const Components: Array<React.ReactNode> = [
-		<GenerateMnemonics />,
-		<CreatePassword />,
-	];
+  const Components: Array<React.ReactNode> = [
+    <GenerateMnemonics setNext={setCurrentComp} />,
+    <CreatePassword />,
+  ];
 
-	return (
-		<div className="bg-cardbg relative flex h-full w-full flex-col items-center justify-center rounded-lg">
-			<ComponentMap
-				className="absolute top-10 w-full p-4"
-				currentComp={currentComp}
-				setNext={setCurrentComp}
-				data={[
-					{ name: "GenerateMnemonics", default: "mute" },
-					{ name: "CreatePassword", default: "mute" },
-				]}
-			/>
-			<motion.div
-				initial={{
-					opacity: 0,
-				}}
-				animate={{
-					opacity: 1,
-					transition: { delay: 0.5 },
-				}}
-				className="h-auto w-auto"
-			>
-				{Components[comIndex]}
-			</motion.div>
-		</div>
-	);
+  return (
+    <div className="bg-cardbg relative flex h-full w-full flex-col items-center justify-center rounded-lg">
+      <ComponentMap
+        currentComp={currentComp}
+        className="absolute top-10 w-full p-4"
+        data={[
+          { name: "GenerateMnemonics", default: "mute" },
+          { name: "CreatePassword", default: "mute" },
+        ]}
+      />
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 0.5 },
+        }}
+        className="h-auto w-auto"
+      >
+        {Components[currentComp]}
+      </motion.div>
+    </div>
+  );
 };
 
 export default NewAccount;
