a5b42b9d7d297b7f9ffed53f3f8cb1ba169e65cc
Praveen Thanikachalam <praveenthanikachalam408@gmail.com>
2025-07-22 23:14:23 +0530

    Handles Rerender 👍

--- src/Components/Template/NewAccount.tsx
+++ src/Components/Template/NewAccount.tsx
@@ -1,45 +1,49 @@
 import CreatePassword from "../Molecules/CreatePassword";
 import GenerateMnemonics from "../Molecules/GenerateMnemonics";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import ComponentMap from "../Molecules/ComponentMap";
 import SelectCoin from "../Molecules/SelectCoin";
 import CreateWallet from "../Molecules/CreateWallet";
 
 const NewAccount = () => {
-  const [currentComp, setCurrentComp] = useState<number>(0);
+	const [currentComp, setCurrentComp] = useState<number>(0);
 
-  const [password, setPassword] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
 
-  const [coin, setCoin] = useState<string>("");
+	const [coin, setCoin] = useState<string>("");
 
-  const [mnemonics, setMnemonics] = useState<string>("");
+	const [mnemonics, setMnemonics] = useState<string>("");
 
-  const Components: Array<React.ReactNode> = [
-    <GenerateMnemonics setMnemonic={setMnemonics} setNext={setCurrentComp} />,
-    <CreatePassword setPassword={setPassword} setNext={setCurrentComp} />,
-    <SelectCoin setCoin={setCoin} setNext={setCurrentComp} />,
-  ];
+	const Components: Array<React.ReactNode> = [
+		<GenerateMnemonics setMnemonic={setMnemonics} setNext={setCurrentComp} />,
+		<CreatePassword setPassword={setPassword} setNext={setCurrentComp} />,
+		<SelectCoin setCoin={setCoin} setNext={setCurrentComp} />,
+	];
 
-  const Data = [
-    { name: "GenerateMnemonics" },
-    { name: "CreatePassword" },
-    { name: "SelectCoin" },
-  ];
+	const FinalStep = useMemo(() => {
+		return <CreateWallet mnemonics={mnemonics} password={password} />;
+	}, [mnemonics, password]);
 
-  return (
-    <div className="bg-cardbg relative flex h-full w-full flex-col items-center justify-center rounded-lg">
-      <ComponentMap
-        currentComp={currentComp}
-        className="absolute top-10 w-full p-4"
-        data={Data}
-      />
-      {currentComp < Data.length ? (
-        Components[currentComp]
-      ) : (
-        <CreateWallet mnemonics={mnemonics} password={password} />
+	const Data = [
+		{ name: "GenerateMnemonics" },
+		{ name: "CreatePassword" },
+		{ name: "SelectCoin" },
+	];
+
+	return (
+		<div className="bg-cardbg relative flex h-full w-full flex-col items-center justify-center rounded-lg">
+			<ComponentMap
+				currentComp={currentComp}
+				className="absolute top-10 w-full p-4"
+				data={Data}
+			/>
+			{currentComp < Data.length ? (
+				Components[currentComp]
+			) : (
+			  FinalStep
       )}
-    </div>
-  );
+		</div>
+	);
 };
 
 export default NewAccount;
