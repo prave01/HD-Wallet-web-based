71b2f79685c79a194ced0a9a71f14ff5511129cd
Praveen Thanikachalam <praveenthanikachalam408@gmail.com>
2025-07-23 23:08:31 +0530

    Minimal

--- src/Actions/CreateWallet.server.ts
+++
@@ -1,5 +0,0 @@
-import Decrypt from "./Crypto/Decrypt.server.ts";
-
-export async function CreateWallet(localData: LocalStorage) {
-  const data = await Decrypt("Praveen@10032004", localData);
-}
--- src/Actions/Crypto/Decrypt.server.ts
+++ src/Actions/Crypto/Decrypt.server.ts
@@ -3,31 +3,30 @@ import bs58 from "bs58";
 import DerivationKey from "./DeriveEncryptionKey.server.ts";
 
 export default async function Decrypt(
-  password: string,
-  cipherObj: LocalStorage,
-): Promise<{ status: number; response: string }> {
-  const cipherText = bs58.decode(cipherObj.cipherText);
-  const iterations = cipherObj.iterations;
-  const salt = bs58.decode(cipherObj.salt);
-  const nonce = bs58.decode(cipherObj.nonce);
-  const keyLength = nacl.secretbox.keyLength;
-  const digest = cipherObj.digest;
+	password: string,
+	cipherObj: LocalStorage,
+): Promise<string> {
+	const cipherText = bs58.decode(cipherObj.cipherText);
+	const iterations = cipherObj.iterations;
+	const salt = bs58.decode(cipherObj.salt);
+	const nonce = bs58.decode(cipherObj.nonce);
+	const keyLength = nacl.secretbox.keyLength;
+	const digest = cipherObj.digest;
 
-  const key = await DerivationKey(
-    password,
-    iterations,
-    keyLength,
-    digest,
-    salt,
-  );
+	const key = await DerivationKey(
+		password,
+		iterations,
+		keyLength,
+		digest,
+		salt,
+	);
 
-  const plainText = nacl.secretbox.open(cipherText, nonce, key);
+	const plainText = nacl.secretbox.open(cipherText, nonce, key);
 
-  if (!plainText) {
-    console.log("Incorrect Password");
-    return { status: 401, response: "Incorrect Password" };
-  }
+	if (!plainText) {
+		throw new Error("Incorrect Password");
+	}
 
-  const mnemonics = Buffer.from(plainText).toString();
-  return { status: 200, response: mnemonics };
+	const mnemonics = Buffer.from(plainText).toString();
+	return mnemonics;
 }
