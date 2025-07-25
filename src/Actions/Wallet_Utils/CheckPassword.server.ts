4b95ed88fb43e5664b5f9cbe98feca8a33bdd05b
Praveen Thanikachalam <praveenthanikachalam408@gmail.com>
2025-07-25 12:57:34 +0530

    Unused function - action to remove

--- src/Actions/Wallet_Utils/CheckPassword.server.ts
+++ src/Actions/Wallet_Utils/CheckPassword.server.ts
@@ -1,12 +1,22 @@
 import { GetAccountPublicKeys } from "./GetKeys.server.ts";
 
-export async function CheckPassword(localData: LocalStorage, password: string) {
-	const getPubKeys = await GetAccountPublicKeys(localData, password);
+export async function CheckPassword(
+	localData: LocalStorage,
+	password: string,
+): Promise<boolean> {
+	console.log("CheckPassword started");
+	try {
+		const getPubKeys = await GetAccountPublicKeys(localData, password);
 
-	if (getPubKeys) {
-		console.log("Logged In ✅");
-		return true;
+		if (getPubKeys) {
+			if (Array.isArray(getPubKeys) && getPubKeys.length > 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return false;
+	} catch (error) {
+		return false;
 	}
-	console.error("Wrong Password ⛔");
-	return false;
 }
